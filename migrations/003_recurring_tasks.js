8cf11e4b08a3c6e92c596b05071f311b8af47ebe
Libor Máčka <macka@fortesinteractive.com>
2026-01-14 22:09:30 +0100

    recurring tasks migration

---
+++ migrations/003_recurring_tasks.js
@@ -0,0 +1,29 @@
+export async function up(knex) {
+  await knex.schema.createTable("tasks_prefab", (table) => {
+    table.increments("id").primary()
+    table.string("name").notNullable()
+    table.string("user").notNullable()
+    table.text("description")
+    table.timestamp("created_at").defaultTo(knex.fn.now())
+    table.timestamp("deleted_at").nullable().defaultTo(null)
+    table.date("deadline")
+    table.enum("priority", ["0", "1", "2"]).notNullable().defaultTo("1")
+  })
+
+  await knex.schema.createTable("tasks_clone", (table) => {
+    table
+    .integer("prefab_id")
+    .unsigned()
+    .notNullable()
+    .references("id")
+    .inTable("tasks_prefab")
+    table.timestamp("scheduled_at").defaultTo(knex.fn.now())
+    table.timestamp("finished_at")
+    table.unique(["prefab_id", "scheduled_for"])
+  })
+}
+
+export async function down(knex) {
+  await knex.schema.dropTableIfExists("tasks_clone")
+  await knex.schema.dropTableIfExists("tasks_prefab")
+}
