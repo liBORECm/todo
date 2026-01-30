ba756d220761a7baa8ea9675b422d408f8c639e0
Libor Máčka <macka@fortesinteractive.com>
2026-01-30 23:22:59 +0100

    HAAAAAA

---
+++ migrations/005_task_prefab_days_nonnullable.js
@@ -0,0 +1,11 @@
+exports.up = async function(knex) {
+  return knex.schema.alterTable("tasks_prefab", (table) => {
+    table.integer("days").notNullable().defaultTo(1).alter()
+  })
+}
+
+exports.down = async function(knex) {
+  return knex.schema.alterTable("tasks_prefab", (table) => {
+    table.integer("days").nullable().defaultTo(null).alter()
+  })
+}
