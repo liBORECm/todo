d37b3bb6c8dc2b5f39714b3e28138f203590af03
Libor Máčka <macka@fortesinteractive.com>
2026-01-31 01:29:07 +0100

    A dost

--- migrations/006_clones_id.js
+++ migrations/006_clones_id.js
@@ -1,13 +1,13 @@
 exports.up = async function(knex) {
   // Přidání id do tasks_clone
   await knex.schema.alterTable("tasks_clone", (table) => {
-    table.increments("id").primary()
+    table.increments("clone_id").primary()
   })
 }
 
 exports.down = async function(knex) {
   // Odebrání id
   await knex.schema.alterTable("tasks_clone", (table) => {
-    table.dropColumn("id")
+    table.dropColumn("clone_id")
   })
 }
