c6f18efd2f19a7e7644a245223c784862fcf3ff3
liBORECm <liborek.macka@seznam.cz>
2026-06-30 23:34:10 +0200

    Added auto gen for swagger and cors

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,24 @@
-import express, { Application, Request, Response } from 'express'
 import dotenv from 'dotenv'
 dotenv.config()
+import express, { Application, Request, Response } from 'express'
+import cors from 'cors'
+import swaggerUi from 'swagger-ui-express'
+import swaggerDocument from './swagger-output.json'
+import todoTableController from './todoTable/todoTable.controller'
 
 const app: Application = express()
 const port = Number(process.env.PORT) || 5533
 
-// Enable URL-encoded form data parsing
-app.use(express.urlencoded({ extended: true }))
-
-// Middleware to parse JSON bodies
 app.use(express.json())
+app.use(cors({ origin: process.env.CORS_ORIGIN, credentials: true }))
+app.use(express.json({ limit: '16kb' }))
+app.use(express.urlencoded({ extended: true, limit: '16kb' }))
+app.use(express.static('public'))
+
+app.use('/api/v1/', todoTableController)
+
+// Serve Swagger documentation
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
 // Basic route
 app.get('/', (req: Request, res: Response) => {
