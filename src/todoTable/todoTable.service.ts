7d4aa362cf14f2ee2b818f9a8ab6a5337547d26e
liBORECm <liborek.macka@seznam.cz>
2026-06-30 21:25:59 +0200

    Todo table service became general abstract CRUD service

--- src/todoTable/todoTable.service.ts
+++ src/todoTable/todoTable.service.ts
@@ -1,40 +1,8 @@
-import { TodoTable } from './todoTabl.emodel'
+import { TodoTable } from './todoTable.model'
 import db from '../db'
 import { Knex } from 'knex'
+import { CRUDService } from '../CRUD/CRUD.service'
 
-class TodoTableService {
-  public async getAll(
-    modifier?: Knex.QueryCallbackWithArgs<any, any>,
-  ): Promise<Array<TodoTable>> {
-    const query = !modifier
-      ? db('todo_tables')
-      : db('todo_tables').modify(modifier)
-    return (await query) as Array<TodoTable>
-  }
+class TodoTableService extends CRUDService<TodoTable> {}
 
-  public async get(id: number): Promise<TodoTable> {
-    return (await db('todo_tables').where('id', id).first()) as TodoTable
-  }
-
-  public async create(table: TodoTable): Promise<boolean> {
-    ;(table as any).id = undefined
-    const result = await db('todo_tables').insert(table)
-    return result.length == 1
-  }
-
-  public async edit(id: number, table: TodoTable): Promise<boolean> {
-    ;(table as any).id = undefined
-    const result = await db('todo_tables').where('id', id).update(table)
-    return result == 1
-  }
-
-  public async delete(id: number): Promise<boolean> {
-    const result = await db('todo_tables').where('id', id).delete()
-
-    return result == 1
-  }
-
-  //const modifier = (query) => query.where()
-}
-
-export default new TodoTableService()
+export default new TodoTableService('todo_tables')
