ef1c9c81086a8f7acea0f80adcc41df0362458cc
liBORECm <liborek.macka@seznam.cz>
2026-07-01 23:23:27 +0200

    New error added

--- src/common/Error.ts
+++ src/common/Error.ts
@@ -5,4 +5,5 @@ interface ErrorCase {
 
 export default {
     NotFound: { status: 404, message: 'Not found' },
+    InternalErro: { status: 500, message: 'Internal error' },
 } as Record<string, ErrorCase>
