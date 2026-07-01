96d60764c6cb118bc24ad73e0e045f394b751e08
liBORECm <liborek.macka@seznam.cz>
2026-07-02 00:14:06 +0200

    Made generic CRUD inheritable controller

---
+++ src/CRUD/CRUD.controller.ts
@@ -0,0 +1,134 @@
+import express, { Router } from 'express'
+import { CRUDService } from './CRUD.service'
+import { Knex } from 'knex'
+import ErrorCase from '../common/Error'
+
+export default abstract class CRUDController<Entity> {
+    service: CRUDService<Entity>
+
+    constructor(service: CRUDService<Entity>) {
+        this.service = service
+    }
+
+    public routes(): Router {
+        const router = express.Router()
+
+        router.get('/', async (req, res) => {
+            const { sort: presort, ...filters } = req.query
+            const sort =
+                !presort || typeof presort !== 'string'
+                    ? undefined
+                    : presort[0] === '-'
+                      ? presort.substring(1)
+                      : presort
+            const sortOrder =
+                !presort || typeof presort !== 'string'
+                    ? undefined
+                    : presort[0] !== '-'
+                      ? 'ASC'
+                      : 'DESC'
+
+            const modifier = (query: Knex.QueryBuilder) => {
+                if (sort && sortOrder) query = query.orderBy(sort, sortOrder)
+
+                for (const [column, value] of Object.entries(filters)) {
+                    query = query.where(column, value)
+                }
+
+                return query
+            }
+
+            try {
+                const result = await this.service.getAll(modifier)
+                return res.status(200).json(result)
+            } catch {
+                const { status, message } = ErrorCase.IntenalError
+                return res.status(status).json({ error: message })
+            }
+        })
+
+        router.get('/:id', async (req, res) => {
+            const id = Number(req.params.id)
+            try {
+                const result = await this.service.get(id)
+                if (result == undefined) {
+                    const { status, message } = ErrorCase.NotFound
+                    return res.status(status).json({ error: message })
+                }
+                return res.status(200).json(result)
+            } catch {
+                const { status, message } = ErrorCase.IntenalError
+                return res.status(status).json({ error: message })
+            }
+        })
+
+        router.patch('/:id', async (req, res) => {
+            const id = Number(req.params.id)
+            const newRecord = req.body as Entity
+            ;(newRecord as any).id = undefined
+
+            try {
+                const result = await this.service.get(id)
+                if (result == undefined) {
+                    const { status, message } = ErrorCase.NotFound
+                    return res.status(status).json({ error: message })
+                }
+
+                const patched = await this.service.edit(id, newRecord)
+                const patchedRecord = await this.service.get(id)
+
+                if (!patched || !patchedRecord) {
+                    const { status, message } = ErrorCase.IntenalError
+                    return res.status(status).json({ error: message })
+                }
+                return res.status(200).json(patchedRecord)
+            } catch {
+                const { status, message } = ErrorCase.IntenalError
+                return res.status(status).json({ error: message })
+            }
+        })
+
+        router.post('/', async (req, res) => {
+            const newRecord = req.body as Entity
+            ;(newRecord as any).id = undefined
+
+            try {
+                const created = this.service.create(newRecord)
+                if (!created) {
+                    const { status, message } = ErrorCase.IntenalError
+                    return res.status(status).json({ error: message })
+                }
+
+                return res.status(200).json(created)
+            } catch {
+                const { status, message } = ErrorCase.IntenalError
+                return res.status(status).json({ error: message })
+            }
+        })
+
+        router.delete('/:id', async (req, res) => {
+            const id = Number(req.params.id)
+
+            try {
+                const result = await this.service.get(id)
+                if (result == undefined) {
+                    const { status, message } = ErrorCase.NotFound
+                    return res.status(status).json({ error: message })
+                }
+
+                const deleted = await this.service.delete(id)
+
+                if (!deleted) {
+                    const { status, message } = ErrorCase.IntenalError
+                    return res.status(status).json({ error: message })
+                }
+                return res.sendStatus(200)
+            } catch {
+                const { status, message } = ErrorCase.IntenalError
+                return res.status(status).json({ error: message })
+            }
+        })
+
+        return router
+    }
+}
