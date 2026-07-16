b59e4dab0712d41e5d1b4e9a81f48766c1a1c6cc
liBORECm <liborek.macka@seznam.cz>
2026-07-16 19:02:27 +0200

    Repeated task instances mdoel added

---
+++ src/rTaskInstance/rTaskInstance.model.ts
@@ -0,0 +1,39 @@
+import { FinishalbeEntity } from '../common/finishable/finishable.model'
+import { RepeatedTask } from '../repeatedTask/repeatedTask.model'
+
+export class RTaskInstanceBase extends FinishalbeEntity {
+    constructor(
+        public id: number,
+        public createdAt: Date,
+        public updatedAt: Date,
+        public deletedAt: Date,
+        public finishedAt: Date,
+        public tableId: number,
+        public repeatedTaskId: number,
+    ) {
+        super(id, createdAt, updatedAt, deletedAt, finishedAt)
+    }
+}
+
+export class RTaskInstance extends RTaskInstanceBase {
+    constructor(
+        public id: number,
+        public createdAt: Date,
+        public updatedAt: Date,
+        public deletedAt: Date,
+        public finishedAt: Date,
+        public tableId: number,
+        public repeatedTaskId: number,
+        public repeatedTask: RepeatedTask,
+    ) {
+        super(
+            id,
+            createdAt,
+            updatedAt,
+            deletedAt,
+            finishedAt,
+            tableId,
+            repeatedTaskId,
+        )
+    }
+}
