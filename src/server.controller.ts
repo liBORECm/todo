6b0be762342f2b5b82c2d87785070323f95752a0
liBORECm <liborek.macka@seznam.cz>
2026-07-23 00:11:22 +0200

    Autoloading controllers

--- src/server.controller.ts
+++ src/server.controller.ts
@@ -1,16 +1,24 @@
-import express, { Router } from 'express'
-import todoTableController from './todoTable/todoTable.controller'
-import simpleTaskController from './simpleTask/simpleTask.controller'
-import repeatedTaskContorller from './repeatedTask/repeatedTask.controller'
-import rTaskInstanceController from './rTaskInstance/rTaskInstance.controller'
+import express from 'express'
+import fs from 'fs'
+import path from 'path'
 
 const router = express.Router()
 
-//TODO make the modules autoload from glob pattern
-
-router.use('/api/v1/', todoTableController)
-router.use('/api/v1/', simpleTaskController)
-router.use('/api/v1/', repeatedTaskContorller)
-router.use('/api/v1/', rTaskInstanceController)
+fs.readdirSync(__dirname, { withFileTypes: true })
+    .filter((entry) => entry.isDirectory() && entry.name !== 'common')
+    .forEach((entry) => {
+        const controllerFile = path.join(
+            __dirname,
+            entry.name,
+            `${entry.name}.controller`,
+        )
+        const exists = ['.ts', '.js'].some((ext) =>
+            fs.existsSync(controllerFile + ext),
+        )
+        if (exists) {
+            const mod = require(controllerFile)
+            router.use('/api/v1/', mod.default ?? mod)
+        }
+    })
 
 export default router
